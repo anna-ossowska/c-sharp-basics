008815dee6b5576feb0e46e20f0def0c26583439
Anna Ossowska <ossowska.anna1@gmail.com>
2021-01-25 14:15:34 +0100

    Operators

---
+++ 2_primitive_types_and_expressions/Operators/Operators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arithmetic Operators
+            //var a = 10;
+            //var b = 3;
+            //Console.WriteLine((float)a / (float)b);
+
+            // Comparison Operators
+            //var a = 1;
+            //var b = 2;
+            //Console.WriteLine(a == b);
+            //Console.WriteLine(a != b);
+
+            // Logical Operators
+            var a = 1;
+            var b = 2;
+            var c = 3;
+            Console.WriteLine(!(a <= 3 && b < 3 || b == c));
+        }
+    }
+}
