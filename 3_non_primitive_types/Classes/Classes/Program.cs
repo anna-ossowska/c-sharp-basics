8775dcaf536d65d5bad6a3c3769961ae6a84238c
Anna Ossowska <ossowska.anna1@gmail.com>
2021-01-25 20:30:50 +0100

    Introducing Classes

---
+++ 3_non_primitive_types/Classes/Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Classes.Math;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var age = calculator.Add(12, 30);
+            Console.WriteLine(age);
+        }
+    }
+}
