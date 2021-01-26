c0144161dfc3e8549469c49f8c789e25a19e774d
Anna Ossowska <ossowska.anna1@gmail.com>
2021-01-26 14:59:43 +0100

    Working with Strings

---
+++ 3_non_primitive_types/Strings/Strings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Anna";
+            var lastName = "Ossowska";
+
+            // String concatenation:
+            var fullName = "Anna" + " " + "Ossowska";
+            Console.WriteLine(fullName);
+
+            // String Format method (composite formatting):
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+            Console.WriteLine(myFullName);
+
+            // Interpolation:
+            Console.WriteLine($"My name is {firstName} {lastName}.");
+
+            // Join method:
+            var names = new string[3] { "John", "Jake", "Mary" };
+            var joinedNames = string.Join(',', names);
+            Console.WriteLine(joinedNames);
+
+            // Verbatim Strings:
+            var path = @"Hi John,
+Look into the following paths:
+c:\folder1\file1
+c:\folder2\file2";
+            Console.WriteLine(path);
+        }
+    }
+}
