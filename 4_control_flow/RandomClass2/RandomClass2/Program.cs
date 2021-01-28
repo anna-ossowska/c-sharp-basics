8673f7024afc2294f3fe9ca592f5fe4b179b40ae
Anna Ossowska <ossowska.anna1@gmail.com>
2021-01-28 23:28:37 +0100

    Exploring the Random Class (Part 2)

---
+++ 4_control_flow/RandomClass2/RandomClass2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RandomClass2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+
+            for (var i = 0; i < passwordLength; i++)
+            {
+                buffer[i] = (char)('a' + random.Next(0, 26));
+            }
+
+            // Taking a random set of characters from an array and storing them in a string
+            // 1st solution:
+            var password = new string(buffer);
+            Console.WriteLine(password);
+
+            // 2nd solution:
+            // var password = String.Join("", buffer);
+        }
+    }
+}
