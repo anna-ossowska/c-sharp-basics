cce9104bb489d851fbdc77e97c5f2fcf43e2ea48
Anna Ossowska <ossowska.anna1@gmail.com>
2021-01-28 19:40:08 +0100

    foreach loop

---
+++ 4_control_flow/ForeachLoop/ForeachLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ForeachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "John Smith";
+
+            // Iterating through the sentence's characters using the for loop
+            for (var i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            // Iterating through the sentence's characters using the foreach
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            // Iterating through an array
+            var numbers = new int[4] { 1, 2, 3, 4 };
+
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
