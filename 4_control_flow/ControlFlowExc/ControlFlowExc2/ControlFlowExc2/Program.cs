4d77bb8c5d4fa296b69ae8fdabc00bee68af166c
Anna Ossowska <ossowska.anna1@gmail.com>
2021-01-29 12:13:42 +0100

    Exercise 2

---
+++ 4_control_flow/ControlFlowExc/ControlFlowExc2/ControlFlowExc2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ControlFlowExc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2 - Write a program and continuously ask the user to enter a number or "ok" to exit.
+            // Calculate the sum of all the previously entered numbers and display it on the console.
+            string input = "";
+            int sum = 0;
+
+            while (input != "ok")
+            {
+                Console.WriteLine("Enter a number: ");
+                Console.WriteLine("Or type 'ok' to exit");
+                input = Console.ReadLine();
+
+                var inputToInt = Convert.ToInt32(input);
+                sum += inputToInt;
+
+                Console.WriteLine($"The sum of typed numbers: {sum}");
+            }
+
+        }
+    }
+}
