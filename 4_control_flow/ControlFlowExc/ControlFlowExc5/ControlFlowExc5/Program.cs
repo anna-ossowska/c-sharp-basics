f633a2308d1d436c74cb268786bf18b9859d083f
Anna Ossowska <ossowska.anna1@gmail.com>
2021-01-29 17:34:12 +0100

    Exercise 5

---
+++ 4_control_flow/ControlFlowExc/ControlFlowExc5/ControlFlowExc5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ControlFlowExc5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5 - Write a program and ask the user to enter a series of numbers separated by comma.
+            //    Find the maximum of the numbers and display it on the console.
+            //    For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
+
+            Console.WriteLine("Enter a series of numbers seperated by comma");
+            string input = Console.ReadLine();
+
+            var arrNum = input.Split(",");
+            Console.WriteLine($"The maximum value is {arrNum.Max(n => Convert.ToInt32(n))}.");
+        }
+    }
+}
+
+
