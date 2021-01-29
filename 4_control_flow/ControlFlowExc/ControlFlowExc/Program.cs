f614b5cad423c03dd7e98de831f7ad01834f439c
Anna Ossowska <ossowska.anna1@gmail.com>
2021-01-29 12:07:38 +0100

    Exercise 1

---
+++ 4_control_flow/ControlFlowExc/ControlFlowExc/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ControlFlowExc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.
+            // Display the count on the console.
+
+            var divisibleNums = new List<int>();
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    divisibleNums.Add(i);
+                }
+            }
+
+            Console.WriteLine($"There are {divisibleNums.Count} numbers divisible by 3 with no remainder.");
+        }
+    }
+}
