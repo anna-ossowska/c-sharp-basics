6a077c3a92d8aa547048444f2a3239b49f0251ec
Anna Ossowska <ossowska.anna1@gmail.com>
2021-01-31 17:03:32 +0100

    Exercise 2

--- 5_arrays_and_lists/Exc/Exercise2/Exercise2/Program.cs
+++ 5_arrays_and_lists/Exc/Exercise2/Exercise2/Program.cs
@@ -10,22 +10,41 @@ namespace Exercise2
             // 2 - Write a program and ask the user to enter their name. Use an array to reverse the name and then store the result in a new string.
             // Display the reversed name on the console.
 
+        // Solution 1 (using Array)
             Console.WriteLine("Enter your name: ");
             var inputName = Console.ReadLine();
-            var charArr = new List<Char>();
+            var charArr = new char[inputName.Length];
 
-            // Add chars into the Array
-            for (int i = 0; i < inputName.Length; i++)
+            for (int i = inputName.Length; i > 0; i--)
             {
-                charArr.Add(inputName[i]);
+                charArr[i - 1] = inputName[inputName.Length - i];
+                
             }
 
-            // Reverse elements inside the Array
-            charArr.Reverse();
+            var reversed = new string(charArr);
+
+            Console.WriteLine(char.ToUpper(reversed[0]) + reversed.Substring(1).ToLower());
+
+
+        // Solution 2 (using List)
+            Console.WriteLine("Enter your name: ");
+            var input = Console.ReadLine();
+            var charList = new List<Char>();
+
+            // Add chars into the List
+            for (int i = 0; i < input.Length; i++)
+            {
+                charList.Add(input[i]);
+            }
+
+            // Reverse elements inside the List
+            charList.Reverse();
 
             // Convert to String
-            var nameReversed = String.Join("", charArr);
+            var nameReversed = String.Join("", charList);
             Console.WriteLine("Reversed name: {0}{1}", char.ToUpper(nameReversed[0]), nameReversed.Substring(1).ToLower());
         }
     }
 }
+
+
