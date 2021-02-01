2b036af7eea247d57bdfbd323e8cc23d4779cef1
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-01 16:19:03 +0100

    StringBuilder Demo

---
+++ 7_working_with_text/StringBuilderDemo/StringBuilderDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder("Hello World");
+
+            // Chaining methods
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('x', 10));
+
+            Console.WriteLine(builder);
+
+            // Get the first character
+            Console.WriteLine("First character: " + builder[0]);
+        }
+    }
+}
