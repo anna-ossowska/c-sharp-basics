4fd5cbd3ff649347699d005b6c6b5779851726e4
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-01 23:35:12 +0100

    Exercise 5

---
+++ 7_working_with_text/Exc/Exercise5/Exercise5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5 - Write a program and ask the user to enter an English word.
+            // Count the number of vowels(a, e, o, u, i) in the word.
+            // So, if the user enters "inadequate", the program should display 6 on the console.
+
+            Console.WriteLine("Enter an English word: ");
+            var input = Console.ReadLine().ToLower();
+            var sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'i' || input[i] == 'e' || input[i] == 'a' || input[i] == 'o' || input[i] == 'u')
+                {
+                    sum++;
+                }        
+            }
+
+            Console.WriteLine($"Your word contains {sum} vowels.");
+        }
+    }
+}
