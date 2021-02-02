7fa2dfa52291b2efb27bb2e3a3efe9745cb8ce4a
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-02 21:45:58 +0100

    Directory and DirectoryInfo Class

---
+++ 9_working_with_files/DirectoryDemo/DirectoryDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ---Directory class provides static methods---
+
+            Directory.CreateDirectory(@"C:\Users\tomas\Desktop\my_files\folder1");
+
+            // "*" means all files
+            // the return type is string[]
+            var files = Directory.GetFiles(@"C:\Users\tomas\Desktop\my_files", "*.docx", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            var directories = Directory.GetDirectories(@"C:\Users\tomas\Documents\anna\repos", "*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            Console.WriteLine(Directory.Exists(@"C:\Users\tomas\Documents\anna\repos"));
+
+
+            // ---DirectoryInfo class provides instance methods---
+
+            var directoryInfo = new DirectoryInfo(@"C:\Users\tomas\Documents\anna\repos");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
