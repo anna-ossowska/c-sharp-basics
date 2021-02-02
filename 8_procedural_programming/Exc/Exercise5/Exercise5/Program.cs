b5184a141b9fcb1cc647039b84f201a053d95faf
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-02 16:10:12 +0100

    Exercise 5

---
+++ 8_procedural_programming/Exc/Exercise5/Exercise5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Rewrite your code using the Procedural Programming paradigm
+
+            //5 - Write a program and ask the user to enter an English word.
+            // Count the number of vowels(a, e, o, u, i) in the word.
+            // So, if the user enters "inadequate", the program should display 6 on the console.
+
+            Console.WriteLine("Enter an English word: ");
+            var input = Console.ReadLine();
+
+            Console.WriteLine($"Your word contains {CountVowels(input)} vowels.");
+        }
+
+        public static string CountVowels(string word)
+        {
+            var input = word.ToLower();
+            var sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'i' || input[i] == 'e' || input[i] == 'a' || input[i] == 'o' || input[i] == 'u')
+                {
+                    sum++;
+                }
+            }
+            return sum.ToString();
+        }
+    }
+}
