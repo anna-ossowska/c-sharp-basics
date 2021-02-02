c3dff7640a8b53da6b6491c22dbac940392c3a4d
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-02 12:05:29 +0100

    Procedural Programming (Demo)

---
+++ 8_procedural_programming/ProceduralProgrammingDemo/ProceduralProgrammingDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PPDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+
+            Console.WriteLine("Reversed name: " + reversed);
+        }
+
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+            {
+                array[name.Length - i] = name[i - 1];
+            }
+
+            var reversed = new string(array);
+
+            return reversed;
+        }
+    }
+}
