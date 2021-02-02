6eec1f39cfe7fa89b69fd2dc2eab23b1e1304d34
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-02 17:37:00 +0100

    File and FileInfo Class

---
+++ 9_working_with_files/FileDemo/FileDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace FileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ---File class provides static methods---
+            var path = @"c:\temp\myfile.jpg";
+
+            // true means - if the file of the same name exists, overwrite it
+            File.Copy(path, @"d:\temp\myfile.jpg", true);
+
+            File.Delete(path);
+
+            var msg = (File.Exists(path)) ? "Exists" : "Does not exist";
+            Console.WriteLine(msg);
+
+            var content = File.ReadAllText(path);
+
+
+            // ---FileInfo class provides instance methods---
+            var fileInfo = new FileInfo(path);
+
+            fileInfo.CopyTo("...");
+
+            fileInfo.Delete();
+
+            var msg2 = fileInfo.Exists ? "Exists" : "Does not exist";
+        }
+    }
+}
