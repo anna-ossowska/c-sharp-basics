de16f45c35a003845f5ed50e82f7c8bb5aa4baa6
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-02 22:17:55 +0100

    Exercise 1

---
+++ 9_working_with_files/Exc/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 - Write a program that reads a text file and displays the number of words.
+
+            var path = @"C:\Users\tomas\Desktop\my_files\folder1\notes.txt";
+
+            var words = CountWordsInFile(path);
+
+            Console.WriteLine($"Your file contains {words} words");
+        }
+
+       public static string CountWordsInFile(string path)
+        {
+            var text = File.ReadAllText(path);
+            return text.Split(" ").Length.ToString();
+        }
+    }
+}
